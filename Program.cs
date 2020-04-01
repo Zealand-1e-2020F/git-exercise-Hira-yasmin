ee49367ab81c5e045c64fec9333d418f8ecbfb0e
Hira-yasmin <hira-yasmin@live.dk>
2020-04-01 13:06:22 +0200

    myclass2
    alt indholdet i myclass2 er slettet nu

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@ namespace GitExercise
         {
             Console.WriteLine("Hello World!");
 
-            myClass myclass1 = new myClass("Hira", "poppelhusene", 2600, "glostrup");
-            myClass myclass2 = new myClass("Yasmin", "hedemarken", 2620, "albertslund");
+            myclass myclass1 = new myclass("Hira", "poppelhusene", 2600, "glostrup");
+            myclass myclass2 = new myclass("Yasmin", "hedemarken", 2620, "albertslund");
             myclass1.ToString();
 
             System.Console.WriteLine(myclass1.ToString());
