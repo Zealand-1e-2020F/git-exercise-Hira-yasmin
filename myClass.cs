f115b24aa990415cf2af029067bfee4cc10ed6e0
Hira-yasmin <hira-yasmin@live.dk>
2020-04-01 10:50:02 +0200

    Update myClass.cs
    tilføjet navn, adresse og postnummer

--- myClass.cs
+++ myClass.cs
@@ -2,6 +2,9 @@ namespace GitExercise
 {
     class myClass
     {
-        
+    public string navn { get; set; }
+    public string adresse { get; set; }
+    public string postnummer { get; set; }
+
     }
 }
