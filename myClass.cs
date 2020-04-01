9f02b5098596c4145db6f17768764629ef16ddac
Hira-yasmin <hira-yasmin@live.dk>
2020-04-01 10:56:19 +0200

    constructor
    tilføjet navn, adresse og postnummer

--- myClass.cs
+++ myClass.cs
@@ -2,9 +2,16 @@ namespace GitExercise
 {
     class myClass
     {
-    public string navn { get; set; }
-    public string adresse { get; set; }
-    public string postnummer { get; set; }
+        public string Navn { get; set; }
+        public string Adresse { get; set; }
+        public string Postnummer { get; set; }
+        public myClass(string navn, string adresse, string postnummer)
+        {
+            Navn = navn;
+            Adresse = adresse;
+            Postnummer = postnummer;
+        }
 
+        }
     }
-}
+        
